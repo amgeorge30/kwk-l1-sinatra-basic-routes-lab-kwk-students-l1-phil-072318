a23469f6da6fbf8f4fd9b2c3e8cfdf2a36c47a77
Anna George <amgeorge30@hotmail.com>
2018-07-31 16:57:38 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,9 +1,18 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+
+#home page
+get '/' do
+  "hello world"
+end
+
+get '/' do
+  "my name is anna"
 end
 
 
-get '/' do
-  "hello world"
+
+
 end
